582ad073e28e5577234e64015fb6dcc48ed54bda
Murph Shaw <u7311689@anu.edu.au>
2024-04-02 23:08:42 +1100

    button animations learning

--- JS/home.js
+++ JS/home.js
@@ -1,6 +1,28 @@
+
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
         section.scrollIntoView({ behavior: 'smooth'});
     }
 }
+
+document.getElementById("menu").addEventListener("click", function(event) {
+    // Prevent the default behavior of the anchor link
+    event.preventDefault();
+    
+    // Get the href value of the anchor tag
+    const linkHref = event.target.getAttribute('href');
+    
+    // Add class for animation
+    event.target.classList.add("animate__animated", "animate__bounce");
+  
+    // Remove animation class after 1 second
+    setTimeout(() => {
+        event.target.classList.remove("animate__animated", "animate__bounce");
+        
+        // Open the link after animation is completed
+        window.open(linkHref, '_blank');
+    }, 1000);
+
+    console.log("Animation applied to the menu button");
+});
